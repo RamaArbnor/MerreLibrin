e8ef3f3fa18b28b8f2dbdbd1eac79b4dd18c71fc
Arbnor Rama <arama@york.citycollege.eu>
2023-09-17 22:34:40 +0200

    quailty changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,48 @@ function App() {
   return (
     <div className="App">
       <Home />
+      
+<footer class="text-center text-lg-start bg-white text-muted">
+  
+  <section class="d-flex justify-content-center justify-content-lg-between p-2 border-bottom">
+
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with me on social networks:</span>
+    </div>
+
+
+  
+    <div>
+      <a href="https://www.facebook.com/arbnor.rama.94/" class="me-4 link-secondary">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="mailto:arma@york.citycollege.eu" class="me-4 link-secondary">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="https://www.instagram.com/arbnor_rama/" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/arbnorrama/" class="me-4 link-secondary">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="https://github.com/ramaArbnor" class="me-4 link-secondary">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+  
+  </section>
+  
+
+    
+  
+  <div class="text-center p-2" style={{backgroundColor: 'rgba(0, 0, 0, 0.025)'}}>
+    © 2023 Copyright :    
+    <a class="text-reset fw-bold" href="https://arbnorrama.com/">Arbnor Rama</a>
+  </div>
+
+</footer>
+
+
     </div>
   );
 }
