1f1bdc3cd046eb4347b78d3ecb2bd8566b6fa569
Arbnor Rama <arama@york.citycollege.eu>
2024-08-25 10:37:25 +0200

    Add books to the data file

--- client/src/data/books.js
+++ client/src/data/books.js
@@ -1,114 +1,498 @@
-export default [{
-    bookName: "Gjuhe Shqipe 1",
-    grade: 1,
-    publisher: "Dukagjini",
-},
-{
-    bookName: "Matematika 1",
-    grade: 1,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Abetare",
-    grade: 1,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Gjuhe Shqipe 2",
-    grade: 2,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Matematika 2",
-    grade: 2,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Gjuhe Shqipe 3",
-    grade: 3,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Matematika 3",
-    grade: 3,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Gjuhe Shqipe 4",
-    grade: 4,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Matematika 4",
-    grade: 4,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Gjuhe Shqipe 5",
-    grade: 5,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Matematika 5",
-    grade: 5,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Gjuhe Shqipe 6",
-    grade: 6,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Matematika 6",
-    grade: 6,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Gjuhe Shqipe 7",
-    grade: 7,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Matematika 7",
-    grade: 7,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Gjuhe Shqipe 8",
-    grade: 8,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Matematika 8",
-    grade: 8,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Gjuhe Shqipe 9",
-    grade: 9,
-    publisher: "Dukagjini",
-}
-,
-{
-    bookName: "Matematika 9",
-    grade: 9,
-    publisher: "Dukagjini",
-}
+export default [
+    {
+        "bookName": "Abetare",
+        "grade": 1,
+        "publisher": "Dukagjini dhe Pegi"
+      },
+      {
+        "bookName": "Gjuha Shqipe 1",
+        "grade": 1,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Angleze – Ping Pong 1",
+        "grade": 1,
+        "publisher": "Hamilton House"
+      },
+      {
+        "bookName": "Edukata Muzikore 1",
+        "grade": 1,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edukata Figurative 1",
+        "grade": 1,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Matematika 1",
+        "grade": 1,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Njeriu dhe natyra 1",
+        "grade": 1,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shoqeria dhe mjedisi 1",
+        "grade": 1,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edukata fizike, sportet dhe shendeti 1",
+        "grade": 1,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shkathtesi per jete 1",
+        "grade": 1,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Shqipe 2",
+        "grade": 2,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Gjuha Angleze – Kids Zone 1",
+        "grade": 2,
+        "publisher": "Hamilton House"
+      },
+      {
+        "bookName": "Edukata Muzikore 2",
+        "grade": 2,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edukata Figurative 2",
+        "grade": 2,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Matematika 2",
+        "grade": 2,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Njeriu dhe natyra 2",
+        "grade": 2,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shoqeria dhe mjedisi 2",
+        "grade": 2,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edukata fizike, sportet dhe shendeti 2",
+        "grade": 2,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shkathtesi per jete 2",
+        "grade": 2,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Shqipe 3",
+        "grade": 3,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Angleze – Kids' Zone 2",
+        "grade": 3,
+        "publisher": "Hamilton House"
+      },
+      {
+        "bookName": "Edukata Muzikore 3",
+        "grade": 3,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edukata Figurative 3",
+        "grade": 3,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Matematika 3",
+        "grade": 3,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Njeriu dhe natyra 3",
+        "grade": 3,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shoqeria dhe mjedisi 3",
+        "grade": 3,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edukata fizike, sportet dhe shendeti 3",
+        "grade": 3,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shkathtesi per jete 3",
+        "grade": 3,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Shqipe 4",
+        "grade": 4,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Angleze – Kids' Zone 3",
+        "grade": 4,
+        "publisher": "Hamilton House"
+      },
+      {
+        "bookName": "Edukata Muzikore 4",
+        "grade": 4,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edukata Figurative 4",
+        "grade": 4,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Matematika 4",
+        "grade": 4,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Njeriu dhe natyra 4",
+        "grade": 4,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shoqeria dhe mjedisi 4",
+        "grade": 4,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edkata fizike, sportet dhe shendeti 4",
+        "grade": 4,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shkathtesi per jete 4",
+        "grade": 4,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Shqipe 5",
+        "grade": 5,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Angleze – Kids' Zone 4",
+        "grade": 5,
+        "publisher": "Hamilton House"
+      },
+      {
+        "bookName": "Edkata Muzikore 5",
+        "grade": 5,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edkata Figurative 5",
+        "grade": 5,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Matematika 5",
+        "grade": 5,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Njeriu dhe natyra 5",
+        "grade": 5,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shoqeria dhe mjedisi 5",
+        "grade": 5,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edkata fizike, sportet dhe shendeti 5",
+        "grade": 5,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Shkathtesi per jete 5",
+        "grade": 5,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Shqipe 6",
+        "grade": 6,
+        "publisher": "Pegi"
+      },
+      {
+        "bookName": "Gjuha Angleze – Kids' Zone 5",
+        "grade": 6,
+        "publisher": "Hamilton House"
+      },
+      {
+        "bookName": "Gjuha Gjermane - Beste Freunde 1 (A1.1)",
+        "grade": 6,
+        "publisher": "Hueber"
+      },
+      {
+        "bookName": "Art Muzikor 6",
+        "grade": 6,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Art Figurativ 6",
+        "grade": 6,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Matematika 6",
+        "grade": 6,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Fizika 6",
+        "grade": 6,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Biologjia 6",
+        "grade": 6,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjeografia 6",
+        "grade": 6,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Historia 6",
+        "grade": 6,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edukata Qytetare 6",
+        "grade": 6,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Edukata Fizike, Sportet dhe Shendeti 6",
+        "grade": 6,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Teknologjia me TIK 6",
+        "grade": 6,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Shqipe 7",
+        "grade": 7,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Gjuha Angleze – Kids' Zone 6",
+        "grade": 7,
+        "publisher": "Hamilton House"
+      },
+      {
+        "bookName": "Art Muzikor 7",
+        "grade": 7,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Art Figurativ 7",
+        "grade": 7,
+        "publisher": "Berati"
+      },
+      {
+        "bookName": "Matematika 7",
+        "grade": 7,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Fizika 7",
+        "grade": 7,
+        "publisher": "Libri Shkollor"
+      },
+      {
+        "bookName": "Biologjia 7",
+        "grade": 7,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Kimia 7",
+        "grade": 7,
+        "publisher": "Libri Shkollor"
+      },
+      {
+        "bookName": "Gjeografia 7",
+        "grade": 7,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Historia 7",
+        "grade": 7,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edkata Qytetare 7",
+        "grade": 7,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edukata Fizike, Sportet dhe Shendeti 7",
+        "grade": 7,
+        "publisher": "Albas"    
+      },
+      {
+        "bookName": "Teknologjia me TIK 7",
+        "grade": 7,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Shqipe 8",
+        "grade": 8,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Gjuha Angleze - Hot Shots 2",
+        "grade": 8,
+        "publisher": "Hamilton House"
+      },
+      {
+        "bookName": "Gjuha Gjermane - Beste Freunde 1 (A1.2)",
+        "grade": 8,
+        "publisher": "Hueber"
+      },
+      {
+        "bookName": "Art Muzikor 8",
+        "grade": 8,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Art Figurativ 8",
+        "grade": 8,
+        "publisher": "Libri Shkollor"
+      },
+      {
+        "bookName": "Matematika 8",
+        "grade": 8,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Fizika 8",
+        "grade": 8,
+        "publisher": "Libri Shkollor"
+      },
+      {
+        "bookName": "Biologjia 8",
+        "grade": 8,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Kimia 8",
+        "grade": 8,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjeografia 8",
+        "grade": 8,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Historia 8",
+        "grade": 8,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Edkata Qytetare 8",
+        "grade": 8,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Edkata Fizike, Sportet dhe Shendeti 8",
+        "grade": 8,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Teknologjia me TIK 8",
+        "grade": 8,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjuha Shqipe 9",
+        "grade": 9,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Gjuha Angleze - Hot Shots 3",
+        "grade": 9,
+        "publisher": "Hamilton House"
+      },
+      {
+        "bookName": "Art Muzikor 9",
+        "grade": 9,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Art Figurativ 9",
+        "grade": 9,
+        "publisher": "Berati"
+      },
+      {
+        "bookName": "Matematika 9",
+        "grade": 9,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Fizika 9",
+        "grade": 9,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Biologjia 9",
+        "grade": 9,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Kimia 9",
+        "grade": 9,
+        "publisher": "Dukagjini"
+      },
+      {
+        "bookName": "Gjeografia 9",
+        "grade": 9,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Historia 9",
+        "grade": 9,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Edkata Qytetare 9",
+        "grade": 9,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Edkata Fizike, Sportet dhe Shendeti 9",
+        "grade": 9,
+        "publisher": "Albas"
+      },
+      {
+        "bookName": "Teknologjia me TIK 9",
+        "grade": 9,
+        "publisher": "Dukagjini"
+      },
 
 ]
